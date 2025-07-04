8810f44a360243e9110aacf87a90b0b575f9ee3b
DiamondOMG <omgdigital.developer.02@gmail.com>
2025-07-04 11:02:34 +0700

    hand_navigate

--- src/lib/Navigate.js
+++ src/lib/Navigate.js
@@ -1,14 +1,31 @@
 "use client";
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function Navigate() {
   const router = useRouter();
+  const pathname = usePathname();
+
+  // กำหนด path ทั้งหมดที่ต้องการให้วน
+  const paths = ["/deer_tummy", "/bigc", "/"];
 
   useEffect(() => {
     window.DigitalSignageTriggerCallback = (data) => {
-      router.push(`/${data}`);
+      if (!data) return;
+      const [cmd, dir] = data.split("_");
+      if (cmd === "hand" && (dir === "right" || dir === "left")) {
+        const currentIdx = paths.indexOf(pathname);
+        if (currentIdx === -1) return;
+        let nextIdx = currentIdx;
+        if (dir === "right") {
+          nextIdx = (currentIdx + 1) % paths.length;
+        } else if (dir === "left") {
+          nextIdx = (currentIdx - 1 + paths.length) % paths.length;
+        }
+        router.push(paths[nextIdx]);
+      }
     };
-  }, [router]);
+  }, [pathname, router, paths]);
+
   return null;
 }
